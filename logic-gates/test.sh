5dd88461808f2c59ae0ad6a3f5d7b82719e748f0
Jesse Palmer <jesselpalmer@gmail.com>
2019-11-29 15:21:42 -0500

    refactor: move files

--- logic-gates/test.sh
+++
@@ -1,6 +0,0 @@
-echo "Executing tests"
-node ./and/and.spec.js
-node ./or/or.spec.js
-node ./nand/nand.spec.js
-node ./nor/nor.spec.js
-echo "Tests completed"
---
+++ test.sh
@@ -0,0 +1,6 @@
+echo "Executing tests"
+node ./logic-gates/and/and.spec.js
+node ./logic-gates/or/or.spec.js
+node ./logic-gates/nand/nand.spec.js
+node ./logic-gates/nor/nor.spec.js
+echo "Tests completed"
