c97049fbb318b49ed2dac03fa696bd0a9a480f51
Jesse Palmer <jesselpalmer@gmail.com>
2019-12-28 00:14:13 -0800

    refactor: put test in alpha order

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 echo "Executing tests"
 node ./logic-gates/and/and.spec.js
 node ./logic-gates/or/or.spec.js
-node ./logic-gates/not/not.spec.js
 node ./logic-gates/nand/nand.spec.js
 node ./logic-gates/nor/nor.spec.js
+node ./logic-gates/not/not.spec.js
 node ./logic-gates/xor/xor.spec.js
 echo "Tests completed"
