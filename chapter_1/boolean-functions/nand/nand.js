ed8a0c6c9db0024df3bb7db9a616c56a317e4eaf
Jesse Palmer <jesselpalmer@gmail.com>
2019-12-28 14:36:01 -0800

    refactor: use helper function

--- chapter_1/boolean-functions/nand/nand.js
+++ chapter_1/boolean-functions/nand/nand.js
@@ -1,5 +1,26 @@
-const and = require('../and')
+const or = require('../or')
+const not = require('../not')
 
-const nand = (a, b) => and(a, b) === 1 ? 0 : 1
+/**
+ * nand
+ * ----
+ * Takes in two binary numbers and returns 0 if both numbers equals one, otherwise returns 1.
+ * 
+ * Truth Table
+ * ------------------------
+ * |  a  |  b  |  result  |
+ * ------------------------
+ * |  0  |  0  |     1    |
+ * |  0  |  1  |     1    |
+ * |  1  |  0  |     1    |
+ * |  1  |  1  |     0    |
+ * ------------------------
+ * 
+ * @param {binary number} a 
+ * @param {binary number} b 
+ * 
+ * @returns binary number
+ */
+const nand = (a, b) => or(not(a), not(b))
 
 module.exports = nand
